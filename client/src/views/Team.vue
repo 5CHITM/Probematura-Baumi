68edd85acb1f3597f90a411e2d874fd45d2a4d97
OnSebii <lang.sebastian.006@gmail.com>
2022-05-01 17:28:11 +0200

    Update Team.vue

--- client/src/views/Team.vue
+++ client/src/views/Team.vue
@@ -16,6 +16,15 @@
     </v-row>
     <v-container>
       <h3>Neugen Agent hier hinzufügen</h3>
+      <p>{{ valid }}</p>
+      <v-form ref="form" v-model="valid" lazy-validation>
+        <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Name" required></v-text-field>
+        <v-text-field v-model="phone" :counter="20" :rules="phoneRules" label="Phone" required></v-text-field>
+        <v-text-field v-model="office" :counter="100" :rules="officeRules" label="Office" required></v-text-field>
+
+        <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate"> Einfügen </v-btn>
+        <v-btn color="error" class="mr-4" @click="reset"> Löschen </v-btn>
+      </v-form>
     </v-container>
   </div>
 </template>
@@ -31,6 +40,32 @@ export default {
       console.log(data);
       this.staffs = data;
     },
+    async addStaff() {
+      // const { data } = await axios('http://127.0.0.1:3000/estates');
+      const { data } = await axios({
+        url: 'http://127.0.0.1:3000/staff',
+        method: 'post',
+        data: {
+          name: this.name,
+          image: 'images/staff/placeholder.jpg',
+          phone: this.phone,
+          office: this.office,
+        },
+      });
+      console.log(data);
+    },
+    async validate() {
+      if (this.$refs.form.validate() == true) {
+        await this.addStaff();
+        await this.getData();
+      }
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation();
+    },
   },
 
   async created() {
@@ -39,8 +74,22 @@ export default {
   data() {
     return {
       staffs: [],
+      valid: true,
+      name: '',
+      nameRules: [(v) => !!v || 'Name is required', (v) => (v && v.length <= 10) || 'Name darf nicht länger als 10 Buchstaben sein'],
+      phone: '',
+      phoneRules: [
+        (v) => !!v || 'Number is required',
+        (v) => /^\+\d\d\(\d{1,5}\)\d+$/.test(v) || 'Die Telefonnummber muss Ländervorwahl und Vorwahl haben',
+        (v) => (v && v.length <= 20) || 'Nummer darf nicht länger als 20 Zeichen sein',
+      ],
+      office: '',
+      officeRules: [
+        (v) => !!v || 'Office is required',
+        (v) => (v && v.length >= 20) || 'Die Officeangabe muss mindestens 20 Zeichen haben',
+        (v) => (v && v.length <= 100) || 'Die Officeangabe darf nicht länger als 100 Zeichen sein',
+      ],
     };
   },
-  setup() {},
 };
 </script>
